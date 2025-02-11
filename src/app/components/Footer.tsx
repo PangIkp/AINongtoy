5258f463ba97b1533eb0704bf20b5433036821af
_shiina <115807888+Drymartini2199@users.noreply.github.com>
2025-02-11 16:28:22 +0700

    update footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,24 +1,39 @@
 "use client";
 import React from 'react'
+import Image from 'next/image';
 
 export default function Footer() {
+    const logo_size = 50;
     return (
-        <div className='absolute inset-x-0 bottom-0 border border-gray-200 w-screen' >
-            <section className='flex justify-around my-16'>
-                <div className='w-1/3 leading-[60px] '>
-                    <p>21 Ladprao Street Bangkok, Thailand</p>
-                    <p>090-846-6758</p>
-                    <p>NongToy@gmail.com</p>
-                </div>
+        <div className='absolute inset-x-0 bottom-0  w-screen bg-[#1B1D36]' >
+            <section className='w-full flex flex-col sm:flex-row sm:justify-around my-6 gap-5 min-w-sm'>
+                <div className='w-full sm:w-1/2 px-5 xl:w-1/3 leading-[70px] '>
+                    <a href="#" className='flex items-center gap-4'>
+                        <Image src="/Images/AINongtoy/Map.png" alt="logo" width={logo_size} height={logo_size} />
+                        <p>21 Ladprao Street Bangkok, Thailand</p>
+                    </a>
+                    <a href="tel:090-846-6758" className='flex items-center gap-4'>
+                        <Image src="/Images/AINongtoy/Phone.png" alt="logo" width={logo_size} height={logo_size} />
+                        <p>090-846-6758</p>
+                    </a>
+                    <a href="mailto:NongToy@gmail.com" className='flex items-center gap-4'>
+                        <Image src="/Images/AINongtoy/Email.png" alt="logo" width={logo_size} height={logo_size} />
+                        <p>NongToy@gmail.com</p>
+                    </a>
 
-                <div className='w-1/3 leading-10'>
+                </div>
+                <div className='w-full sm:w-1/2 px-5 xl:w-1/3 leading-10'>
                     <p className='font-bold'>About the company</p>
                     <p>We use AI to revolutionize Art Toy design, making 3D modeling easier and faster. Our system supports real-world production, turning your ideas into tangible creations. Transform your imagination into reality with cutting-edge technology!</p>
-
+                    <div className='flex gap-4 mt-4'>
+                        <a href="#"><Image src="/Images/AINongtoy/Facebook.png" alt="Facebook logo" width={logo_size} height={logo_size} /><p className='hidden'>Facebook</p></a>
+                        <a href="#"><Image src="/Images/AINongtoy/Instagram.png" alt="Instagram logo" width={logo_size} height={logo_size} /><p className='hidden'>Instagram</p></a>
+                        <a href="#"><Image src="/Images/AINongtoy/Twitter.png" alt="Twitter logo" width={logo_size} height={logo_size} /><p className='hidden'>Twitter</p></a>
+                    </div>
                 </div>
             </section>
 
-            <div className='bg-[#20255E] text-white  w-full h-[52px] flex justify-center items-center'>
+            <div className='bg-[#20255E] text-white  w-full h-[52] flex justify-center items-center'>
                 <p>© 2025 NongToy. All rights reserved.</p>
             </div>
         </div>
