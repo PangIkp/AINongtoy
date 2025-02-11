0111afac7913aa6cfc9ba97273b13aae67aebca8
Rangtude <panggylovely@gmail.com>
2025-02-11 12:06:19 +0700

    update navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,19 +6,6 @@ import { Menu, X } from "lucide-react";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
-
-  // ตรวจสอบขนาดหน้าจอเมื่อโหลดหน้าเว็บ
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 1000);
-    };
-
-    handleResize(); // ตรวจสอบตอนโหลดครั้งแรก
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
 
   return (
     <nav className="fixed top-0 left-0 w-full flex items-center justify-between px-8 md:px-20 py-6 bg-[#010312] text-white drop-shadow-lg z-50">
@@ -38,7 +25,7 @@ export default function Navbar() {
       </div>
 
       {/* Menu ปกติ */}
-      <ul className={`hidden md:flex space-x-[80px] text-[16px] font-semibold`}>
+      <ul className={`hidden lg:flex space-x-[80px] text-[16px] font-semibold`}>
         {["Create Art Toys", "About Us", "Partners", "Contact"].map((item) => (
           <li key={item}>
             <Link href={`/${item.toLowerCase().replace(/\s/g, "-")}`}>
@@ -51,16 +38,16 @@ export default function Navbar() {
       </ul>
 
       {/* Hamburger Menu Button */}
-      {isMobile && (
-        <button className="md:hidden text-white" onClick={() => setMenuOpen(!menuOpen)}>
+ 
+        <button className="lg:hidden text-white" onClick={() => setMenuOpen(!menuOpen)}>
           {menuOpen ? <X size={32} /> : <Menu size={32} />}
         </button>
-      )}
+ 
 
       {/* เมนูสำหรับมือถือ */}
       {menuOpen && (
-        <ul className="absolute top-[80px] left-0 w-full bg-[#010312] flex flex-col items-center space-y-6 py-6 md:hidden">
-          {["Create Art Toys", "About Us", "Partners", "Contact"].map((item) => (
+        <ul className="absolute top-[80px] left-0 w-full bg-[#010312] flex flex-col items-center space-y-6 py-6 lg:hidden">
+          {["Create Art Toys", "About Us", "Partners", "Contact", "Login"].map((item) => (
             <li key={item}>
               <Link
                 href={`/${item.toLowerCase().replace(/\s/g, "-")}`}
@@ -75,7 +62,7 @@ export default function Navbar() {
       )}
 
       {/* Login Button */}
-      <Link href="/login" className="hidden md:block">
+      <Link href="/login" className="hidden lg:block">
         <button className="bg-[#0AACF0] hover:bg-[#0578AB] text-white font-extrabold px-6 py-1 rounded-[5px] text-[15px] transition-all">
           Login
         </button>
