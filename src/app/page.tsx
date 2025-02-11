8b528bd1c7def1f3e2b2f949481786590e9eeb66
_shiina <115807888+Drymartini2199@users.noreply.github.com>
2025-02-11 12:33:55 +0700

    Revert "Merge branch 'dev' of https://github.com/PangIkp/AINongtoy into aek"
    This reverts commit be7388dff4bf9aa57835199e26edeca484221dde, reversing
    changes made to 28037367648ed9beab886cee69638c7e5ffb3eac.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -7,19 +6,6 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Navbar />
       <Footer />
-      <div className="">
-        <Navbar />
-        <div className="relative w-full h-auto">
-          <Image
-            src="/Images/AINongtoy/mainbg.png"
-            alt="mainbg"
-            width={1920} // กำหนดอัตราส่วน
-            height={600}
-            className="object-cover w-full"
-            priority
-          />
-        </div>
-      </div>
     </div>
   );
 }
