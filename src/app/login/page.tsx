9c786685526f5a0a3138ddec97d4e7209d8256db
_shiina <115807888+Drymartini2199@users.noreply.github.com>
2025-02-14 16:32:45 +0700

    update profile

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,7 @@ const Login = () => {
     return (
         <div>
             <Navbar />
-            <div className="w-full h-[90vh]">
+            <div className="w-full h-[90vh] mt-[75px]">
                 {/* ภาพพื้นหลัง */}
                 <div className="relative w-full h-full flex items-center justify-center">
                     {" "}
@@ -19,7 +19,7 @@ const Login = () => {
                         className="object-cover"
                         priority
                     />
-                    <div className="absolute max-w-[500px] w-full p-5 ">
+                    <div className="absolute max-w-[375px] w-full p-4">
                         <div>
                             <h1 className='text-4xl font-semibold mb-3'>Welcome back</h1>
                             <p className='font-extralight'>Please enter your details</p>
@@ -32,15 +32,15 @@ const Login = () => {
                                 <label className='hidden' htmlFor="password">Password</label>
                                 <input className='block' type="password" id='password' placeholder='Password' />
 
-                                <div className='flex justify-between'>
+                                <div className='flex justify-between text-[13px]'>
                                     <label htmlFor="remember" className='flex items-center gap-2 cursor-pointer'>
-                                        <input className='scale-125 accent-black ' type="checkbox" id='remember' />
+                                        <input className='scale-125 accent-black focus:ring-black' type="checkbox" id='remember' />
                                         Remember&nbsp;me
                                     </label>
                                     <a href="#" className='text-[#0AACF0]'>Forgot password ?</a>
                                 </div>
-                                <button className='h-[50px]'>Login</button>
-                                <div className='flex justify-center'>
+                                <button className='h-[40px]'>Login</button>
+                                <div className='flex justify-center text-[13px]'>
                                     <p>Not registered yet ? <a href="/signup" className='text-[#0AACF0]'>Sign up</a></p>
                                 </div>
                             </form>
