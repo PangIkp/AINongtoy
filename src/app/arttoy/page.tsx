594054b682c216f720d61e4f84cf5ca92b033328
Rangtude <panggylovely@gmail.com>
2025-02-17 20:41:41 +0700

    update art toy page

--- src/app/arttoy/page.tsx
+++ src/app/arttoy/page.tsx
@@ -1,17 +1,30 @@
 "use client";
-import { useRouter, usePathname } from "next/navigation";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Image from "next/image";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Filter from "../components/Filter";
 import ArttoyCard from "../components/ArttoyCard";
 import Search from "../components/Search";
+import { usePollinationsImage } from "@pollinations/react";
 
 export default function Arttoy() {
   const aboutRef = useRef<HTMLDivElement>(null!);
   const partnerRef = useRef<HTMLDivElement>(null!);
   const contactRef = useRef<HTMLDivElement>(null!);
+
+  const [prompt, setPrompt] = useState("cat anime");
+
+  // Generate 20 images dynamically
+  const imageUrls = Array.from({ length: 20 }, (_, i) =>
+    usePollinationsImage(prompt, {
+      width: 300,
+      height: 300,
+      seed: 2 + i * 2, // ใช้ seed ที่แตกต่างกัน
+      model: "flux",
+      nologo: true,
+    })
+  );
 
   const scrollToSection = (ref: React.RefObject<HTMLDivElement | null>) => {
     if (ref.current) {
@@ -29,7 +42,6 @@ export default function Arttoy() {
       />
 
       <div className="relative mt-20 w-full h-[256px] flex justify-center items-center">
-        {/* รูปภาพพื้นหลัง */}
         <Image
           src="/Images/AINongtoy/mainbg.png"
           alt="mainbg"
@@ -37,8 +49,6 @@ export default function Arttoy() {
           className="object-cover"
           priority
         />
-
-        {/* ข้อความที่อยู่กลางภาพ */}
         <div className="absolute inset-0 flex flex-col justify-center items-center text-white font-semibold text-center h-full">
           <h1 className="text-[clamp(30px,5vw,45px)] drop-shadow-lg">
             Create Unique Art Toys - Turn your
@@ -51,9 +61,12 @@ export default function Arttoy() {
 
       <div className="flex justify-between pl-10">
         <Filter />
-        <div className="w-full">
-            <Search/>
-        <ArttoyCard/>
+        <div className="bg-[#51536D] w-full">
+          <Search />
+          <div className="p-10">
+            {/* ส่ง imageUrls ไปที่ ArttoyCard */}
+            <ArttoyCard imageUrls={imageUrls} />
+          </div>
         </div>
       </div>
 
