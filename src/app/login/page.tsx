9eea2a992abf51d1206682c7cd930ea33a4ff933
_shiina <115807888+Drymartini2199@users.noreply.github.com>
2025-02-21 19:00:15 +0700

    format input

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,37 @@
 "use client";
 import React, { useState, useEffect, useRef } from 'react';
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import Image from 'next/image'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Image from 'next/image';
 
 const Login = () => {
     const [isClient, setIsClient] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [usernameError, setUsernameError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
 
     useEffect(() => {
         setIsClient(true);
     }, []);
+
+    useEffect(() => {
+        const usernamePattern = /^[A-Za-z][A-Za-z0-9._]{3,}$/;
+        if (username && !usernamePattern.test(username)) {
+            setUsernameError('Username: 4-20 chars, letters, numbers, ., _ only');
+        } else {
+            setUsernameError('');
+        }
+    }, [username]);
+
+    useEffect(() => {
+        const passwordPattern = /^[A-Za-z][A-Za-z0-9._]{7,}$/;
+        if (password && !passwordPattern.test(password)) {
+            setPasswordError('Password: More than 8 characters, letters, numbers, ., _ only.');
+        } else {
+            setPasswordError('');
+        }
+    }, [password]);
 
     const aboutRef = useRef<HTMLDivElement>(null!);
     const partnerRef = useRef<HTMLDivElement>(null!);
@@ -25,10 +47,7 @@ const Login = () => {
         <div>
             <Navbar scrollToSection={scrollToSection} aboutRef={aboutRef} partnerRef={partnerRef} contactRef={contactRef} />
             <div className="w-full h-[90vh] mt-[5rem]">
-                {/* ภาพพื้นหลัง */}
                 <div className="relative w-full h-full flex items-center justify-center">
-                    {" "}
-                    {/* กำหนดขนาดสูง */}
                     {isClient && (
                         <Image
                             src="/Images/AINongtoy/mainbg.png"
@@ -44,12 +63,27 @@ const Login = () => {
                             <p className='font-extralight'>Please enter your details</p>
                         </div>
                         <div>
-                            <form action="" className='flex flex-col justify-between gap-4 w-full h-[70%]  pt-5'>
+                            <form action="" className='flex flex-col justify-between gap-4 w-full h-[70%] pt-5'>
                                 <label className='hidden' htmlFor="username"><p>Username</p></label>
-                                <input className='block' type="text" id='username' placeholder='Username' />
+                                <input
+                                    className='block'
+                                    type="text"
+                                    id='username'
+                                    placeholder='Username'
+                                    maxLength={20}
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
 
                                 <label className='hidden' htmlFor="password"><p>Password</p></label>
-                                <input className='block' type="password" id='password' placeholder='Password' />
+                                <input
+                                    className='block'
+                                    type="password"
+                                    id='password'
+                                    placeholder='Password'
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
 
                                 <div className='flex justify-between text-[13px]'>
                                     <label htmlFor="remember" className='flex items-center gap-2 cursor-pointer'>
@@ -63,12 +97,17 @@ const Login = () => {
                                     <p>Not registered yet ? <a href="/signup" className='text-[#0AACF0]'>Sign up</a></p>
                                 </div>
                             </form>
+                            <div className='mt-4 h-[40px] text-[11px] font-thin'>
+                                <p>{usernameError}</p>
+                                <p>{passwordError}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <Footer />
         </div>
-    )
-}
-export default Login
+    );
+};
+
+export default Login;
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Image from 'next/image';
@@ -41,45 +41,41 @@ const Signup = () => {
         setFormData({ ...formData, [id]: value });
     };
 
+    useEffect(() => {
+        validateForm();
+    }, [formData]);
+
     const validateForm = () => {
         let valid = true;
         let newErrors = { ...errors };
 
-        if (!formData.fname) {
-            newErrors.fname = 'First name is required';
-            valid = false;
-        } else if (formData.fname.length < 2 || formData.fname.length > 50) {
-            newErrors.fname = 'First name must be between 2 and 50 characters';
+        const namePattern = /^[A-Za-z][A-Za-z0-9._]*$/;
+        const usernamePattern = /^[A-Za-z][A-Za-z0-9._]*$/;
+        const passwordPattern = /^[A-Za-z][A-Za-z0-9._]*$/;
+
+        if (formData.fname && (!namePattern.test(formData.fname) || formData.fname.length < 4)) {
+            newErrors.fname = 'First name: 4-20 chars, letters, ., _ only.';
             valid = false;
         } else {
             newErrors.fname = '';
         }
 
-        if (!formData.lname) {
-            newErrors.lname = 'Last name is required';
-            valid = false;
-        } else if (formData.lname.length < 2 || formData.lname.length > 50) {
-            newErrors.lname = 'Last name must be between 2 and 50 characters';
+        if (formData.lname && (!namePattern.test(formData.lname) || formData.lname.length < 4)) {
+            newErrors.lname = 'Last name : 4-20 chars, letters, ., _ only.';
             valid = false;
         } else {
             newErrors.lname = '';
         }
 
-        if (!formData.username) {
-            newErrors.username = 'Username is required';
-            valid = false;
-        } else if (formData.username.length < 3 || formData.username.length > 30) {
-            newErrors.username = 'Username must be between 3 and 30 characters';
+        if (formData.username && (!usernamePattern.test(formData.username) || formData.username.length < 4)) {
+            newErrors.username = 'Username : 4-20 chars, letters, ., _ only.';
             valid = false;
         } else {
             newErrors.username = '';
         }
 
         const phonePattern = /^[0-9]{10}$/;
-        if (!formData.phone) {
-            newErrors.phone = 'Phone number is required';
-            valid = false;
-        } else if (!phonePattern.test(formData.phone)) {
+        if (formData.phone && !phonePattern.test(formData.phone)) {
             newErrors.phone = 'Phone number is invalid';
             valid = false;
         } else {
@@ -87,21 +83,15 @@ const Signup = () => {
         }
 
         const emailPattern = /^[^\s@]+@gmail\.com$/;
-        if (!formData.email) {
-            newErrors.email = 'Email address is required';
-            valid = false;
-        } else if (!emailPattern.test(formData.email)) {
+        if (formData.email && !emailPattern.test(formData.email)) {
             newErrors.email = 'Email address must be a valid @gmail.com address';
             valid = false;
         } else {
             newErrors.email = '';
         }
 
-        if (!formData.password) {
-            newErrors.password = 'Password is required';
-            valid = false;
-        } else if (formData.password.length < 6 || formData.password.length > 20) {
-            newErrors.password = 'Password must be between 6 and 20 characters';
+        if (formData.password && (!passwordPattern.test(formData.password) || formData.password.length < 8)) {
+            newErrors.password = 'Password: More than 8 characters, letters, numbers, ., _ only..';
             valid = false;
         } else {
             newErrors.password = '';
@@ -129,7 +119,7 @@ const Signup = () => {
     return (
         <div>
             <Navbar scrollToSection={scrollToSection} aboutRef={aboutRef} partnerRef={partnerRef} contactRef={contactRef} />
-            <div className="w-full h-[90vh] mt-[5rem]">
+            <div className="w-full sm:h-[90vh] h-[925px] mt-[5rem]">
                 <div className="relative w-full h-full flex items-center justify-center">
                     <Image
                         src="/Images/AINongtoy/mainbg.png"
@@ -148,42 +138,51 @@ const Signup = () => {
                                 <div className='grid grid-rows-2 sm:grid-cols-2 sm:grid-rows-1 gap-5'>
                                     <label htmlFor="fname">
                                         <p>First name</p>
-                                        <input type="text" id='fname' value={formData.fname} onChange={handleChange} />
-                                        <p className="text-red-500 text-sm">{errors.fname || ''}</p>
+                                        <input type="text" id='fname' maxLength={20} value={formData.fname} onChange={handleChange} />
+
                                     </label>
                                     <label htmlFor="lname">
                                         <p>Last name</p>
-                                        <input type="text" id='lname' value={formData.lname} onChange={handleChange} />
-                                        <p className="text-red-500 text-sm">{errors.lname || ''}</p>
+                                        <input type="text" id='lname' maxLength={20} value={formData.lname} onChange={handleChange} />
+
                                     </label>
                                 </div>
 
                                 <div className='grid grid-rows-2 sm:grid-cols-2 sm:grid-rows-1 gap-5'>
                                     <label htmlFor="username">
                                         <p>Username</p>
-                                        <input type="text" id='username' value={formData.username} onChange={handleChange} />
-                                        <p className="text-red-500 text-sm">{errors.username || ''}</p>
+                                        <input type="text" id='username' maxLength={20} value={formData.username} onChange={handleChange} />
                                     </label>
                                     <label htmlFor="phone">
                                         <p>Phone number</p>
-                                        <input type="text" id='phone' value={formData.phone} onChange={handleChange} />
-                                        <p className="text-red-500 text-sm">{errors.phone || ''}</p>
+                                        <input type="text" id='phone' maxLength={10} value={formData.phone} onChange={handleChange} />
+
                                     </label>
                                 </div>
 
                                 <label htmlFor="email">
                                     <p>Email address</p>
-                                    <input type="email" id='email' value={formData.email} onChange={handleChange} />
-                                    <p className="text-red-500 text-sm">{errors.email || ''}</p>
+                                    <input type="email" id='email' maxLength={50} value={formData.email} onChange={handleChange} />
+
                                 </label>
 
-                                <div className='grid grid-rows-2 sm:grid-cols-2 sm:grid-rows-1 gap-x-5'>
+                                <div className='grid grid-rows-2 sm:grid-cols-2 sm:grid-rows-1 gap-x-5 gap-5'>
                                     <PasswordInput id="password" label="Password" value={formData.password} onChange={handleChange} />
                                     <PasswordInput id="cpassword" label="Confirm password" value={formData.cpassword} onChange={handleChange} />
-                                    <p className="text-red-500 text-sm">{errors.cpassword || ''}</p>
+
                                 </div>
                                 <button className='h-[40px]'>Sign Up</button>
                             </form>
+
+                            <div className='mt-4 h-[7.375rem] text-[11px] font-thin'>
+                                {errors.fname && <p>{errors.fname}</p>}
+                                {errors.lname && <p>{errors.lname}</p>}
+                                {errors.username && <p>{errors.username}</p>}
+                                {errors.phone && <p>{errors.phone}</p>}
+                                {errors.email && <p>{errors.email}</p>}
+                                {errors.password && <p>{errors.password}</p>}
+                                {errors.cpassword && <p>{errors.cpassword}</p>}
+                            </div>
                         </div>
                     </div>
                 </div>
