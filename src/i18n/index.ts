56d4ebb0d2182211eccacf7bf6c32c15d0bce101
Rangtude <panggylovely@gmail.com>
2025-04-11 01:19:50 +0700

    fix: update i18n configuration to use local translation resources and adjust tsconfig paths

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,21 +1,24 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import HttpBackend from "i18next-http-backend";
+// import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+import enTranslation from "@/locales/en/translation.json";
+import thTranslation from "@/locales/th/translation.json";
+
 i18n
-    .use(HttpBackend) // โหลดไฟล์ JSON สำหรับการแปล
-    .use(LanguageDetector) // ตรวจจับภาษาของผู้ใช้
-    .use(initReactI18next) // ใช้ร่วมกับ React
-    .init({
-        fallbackLng: "en", // ภาษาเริ่มต้น
-        debug: true,
-        interpolation: {
-            escapeValue: false, // React จะจัดการการ escape เอง
-        },
-        backend: {
-            loadPath: "/locales/{{lng}}/translation.json", // ที่อยู่ไฟล์แปล
-        },
-    });
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: { translation: enTranslation },
+      th: { translation: thTranslation },
+    },
+  });
 
 export default i18n;
